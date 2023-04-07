e1bea05be9d6f5976d528637564b3add89eb13f0
Edil Usobaiev <usobaiev1@gmail.com>
2023-04-07 21:30:54 +0600

    Add .gitinore, Readme.md, data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+const players = [
+	{name: 'C.Ronaldo',  country: "Portugal",  club: "Al-Nasr", position: "ST"},
+	{name: 'L.Messi', country: "Argentina", club: "Paris Saint-Germain", position: "RW"},
+	{name: 'Neymar.Jr', country: "Brazil", club: "Paris Saint-Germain", position: "LW"},
+]
+
+module.exports = players;
